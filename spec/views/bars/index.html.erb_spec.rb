5cbb784745c754f0189736dc9324618e9ecbb219
Evan Dorn <evan@lrdesign.com>
2010-03-21 22:28:32 -0700

    two initial views to play with

--- spec/views/bars/index.html.erb_spec.rb
+++ spec/views/bars/index.html.erb_spec.rb
@@ -5,8 +5,7 @@ describe "/bars/index" do
     render 'bars/index'
   end
 
-  #Delete this example and add some real ones or delete this file
-  it "should tell you where to find the file" do
-    response.should have_tag('p', %r[Find me in app/views/bars/index])
+  it "should succeed" do
+    response.should be_success
   end
 end
